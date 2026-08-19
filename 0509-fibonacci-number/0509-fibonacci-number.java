c0a653193070341bdfac1ae801efc0709b64548d
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-08-19 20:47:14 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,24 @@
 class Solution {
-    public int fib(int n) {
-        if(n<=1){
-          return n;
-        }
 
-        int result = fib(n-1) + fib(n-2);
-        return result;
+  /** Fibonacci using Memoization */
+
+  static int dp[];
+
+  public int helper(int n) {
+    if (n <= 1) {
+      return n;
     }
+    if (dp[n] != 0) {
+      return dp[n];
+    }
+
+    int answer = helper(n - 1) + helper(n - 2);
+    dp[n] = answer;
+    return answer;
+  }
+
+  public int fib(int n) {
+    dp = new int[n + 1];
+    return helper(n);
+  }
 }
