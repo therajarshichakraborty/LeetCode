42bb51f982b16b18e4e45f3ffbef4c9267f6882d
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-08-24 16:06:36 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -17,9 +17,23 @@ class Solution {
     return dp[n] = Math.max(take, skip);
   }
 
+  /** House Robber using Tabulation */
+
   public int rob(int[] nums) {
-    dp = new int[nums.length];
+    int n = nums.length;
+    if (n == 1) {
+      return nums[0];
+    }
+    dp = new int[n];
     Arrays.fill(dp, -1);
-    return helper(nums, 0, dp);
+
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (int i = 2; i < n; i++) {
+      dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+
+    return dp[n - 1];
   }
 }
